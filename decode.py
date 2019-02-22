ee24ea43f08601cdb6cbe2434fe3304c4afcd37a
andthenenteredalex <30809185+andthenenteredalex@users.noreply.github.com>
2019-02-22 18:32:20 -0500

    Uploading url encode and decode python scripts

---
+++ decode.py
@@ -0,0 +1,10 @@
+# completed 11/18/2018 
+# copyright Alex Clark
+# decodes a url encoded string
+# usage: python3 decode.py %20some%20string%20this%20is%21%20yay%21%20
+
+from urllib.parse import unquote
+from sys import argv
+
+print(unquote(u'%s' % argv[1]))
+
---
+++ encode.py
@@ -0,0 +1,10 @@
+# completed 2/22/2019
+# copyright Alex Clark
+# url encodes a plaintext string
+# usage: python3 encode.py 'hello, there, mr. grinch!'
+
+from urllib.request import quote 
+from sys import argv
+
+print(quote(u'%s' % argv[1]))
+
