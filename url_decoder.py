1ed44beb2e9cbc591ec23216279d4199bcb43e8d
andthenenteredalex <30809185+andthenenteredalex@users.noreply.github.com>
2019-02-22 18:51:24 -0500

    Update url_decoder.py

--- url_decoder.py
+++ url_decoder.py
@@ -1,5 +1,5 @@
 # decodes a url encoded string
-# usage: python3 decode.py %20some%20string%20this%20is%21%20yay%21%20
+# usage: python3 url_decoder.py %20some%20string%20this%20is%21%20yay%21%20
 
 from urllib.parse import unquote
 from sys import argv
