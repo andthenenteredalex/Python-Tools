7951bfc76b561bb8363784fcc313320b7b460d15
andthenenteredalex <30809185+andthenenteredalex@users.noreply.github.com>
2019-02-22 18:34:02 -0500

    Update url_decode.py

--- url_decode.py
+++ url_decode.py
@@ -1,5 +1,4 @@
-# completed 11/18/2018 
-# copyright Alex Clark
+# completed 11/18/2018 by Alex Clark
 # decodes a url encoded string
 # usage: python3 decode.py %20some%20string%20this%20is%21%20yay%21%20
 
@@ -7,4 +6,3 @@ from urllib.parse import unquote
 from sys import argv
 
 print(unquote(u'%s' % argv[1]))
-
