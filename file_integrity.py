717760485f427460c0a23b599016d0fc5e156fbc
Eddie Knight <iv.eddieknight@gmail.com>
2019-06-21 21:06:58 -0400

    WIP:
    Incomplete changes to make the script more object oriented

--- file_integrity.py
+++ file_integrity.py
@@ -9,50 +9,60 @@ from os import walk
 import hashlib
 import csv
 
-mydirectory = r'C:\users\85510\security-tools' # directory for integrity check. checks each file in this directory
-hash_time_stamp = str(strftime("%Y-%m-%d %H:%M:%S", gmtime())) # timestamp
+class file_hasher():
+	
+	def __init__(self):
+		self.hash_time_stamp = str(strftime("%Y-%m-%d %H:%M:%S", gmtime())) # timestamp
+		self.all_files_found_in_specified_folder = []
+		self.file_names_in_directory = []
+		
+	def run(mydirectory, io_file_name):
+		self.mydirectory = mydirectory
+		self.get_filenames()
+		self.write_results(io_file_name)
+		
+		
+	def get_filenames():
+		for (dirpath, dirnames, filenames) in walk(self.mydirectory):
+		    self.file_names_in_directory.extend(filenames)
+		    break
 
-all_files_found_in_specified_folder = []
+	def write_results(filename):
+		# open file with hashes from last run
+		with open(filename, 'r') as file:
+		    reader = csv.reader(file, delimiter='=')
+		    old_hashes = list(reader) # old_hashes stores in old_hashes variable
+		file.close()
 
-for (dirpath, dirnames, filenames) in walk(mydirectory):
-    file_names_in_directory.extend(filenames)
-    break
+		# writing new hashes to hash-outfile2.txt
+		hash_outfile2 = open('hash-outfile2.txt', 'w')
+		for item in all_files_found_in_specified_folder: # computing hashes
+		    item2 = r'%s\%s' % (mydirectory,item)
+		    with open(item2, "rb") as file:
+			bytes = file.read()
+			readable_hash = hashlib.sha256(bytes).hexdigest()
+			item_var = '%s=%s\n' % (item2, readable_hash)
+			hash_outfile2.write(item_var)		
+		hash_outfile2.close()
 
-# open file with hashes from last run
-with open('hash-outfile2.txt', 'r') as hash_outfile2:
-    reader = csv.reader(hash_outfile2, delimiter='=')
-    old_hashes = list(reader) # old_hashes stores in old_hashes variable
-hash_outfile2.close()
+	# open new hashes to compare to old hashes
+	with open('hash-outfile2.txt', 'r') as hash_outfile2:
+	    reader2 = csv.reader(hash_outfile2, delimiter='=')
+	    new_hashes = list(reader2) # new hashes stored in new_hashes variable
+	hash_outfile2.close()
 
-# writing new hashes to hash-outfile2.txt
-hash_outfile2 = open('hash-outfile2.txt', 'w')
-for item in all_files_found_in_specified_folder: # computing hashes
-    item2 = r'%s\%s' % (mydirectory,item)
-    with open(item2, "rb") as file:
-        bytes = file.read()
-        readable_hash = hashlib.sha256(bytes).hexdigest()
-        item_var = '%s=%s\n' % (item2, readable_hash)
-        hash_outfile2.write(item_var)		
-hash_outfile2.close()
+	print('''#########################################################''')
+	print('''######`     )###(         )####/  \#####/  \######   ####''')
+	print('''#####/    #########|   |######`    #####    `#####   ####''')
+	print('''(          )#######|   |######      ###      #####   ####''')
+	print('''#####+   +#########|   |######   #   #   #   #####   ####''')
+	print('''######   ##########|   |######   ##\   /##   ############''') 
+	print('''######   #######(         )###   #########   #####   ####''')
+	print('''#########################################################''')
 
-# open new hashes to compare to old hashes
-with open('hash-outfile2.txt', 'r') as hash_outfile2:
-    reader2 = csv.reader(hash_outfile2, delimiter='=')
-    new_hashes = list(reader2) # new hashes stored in new_hashes variable
-hash_outfile2.close()
-
-print('''#########################################################''')
-print('''######`     )###(         )####/  \#####/  \######   ####''')
-print('''#####/    #########|   |######`    #####    `#####   ####''')
-print('''(          )#######|   |######      ###      #####   ####''')
-print('''#####+   +#########|   |######   #   #   #   #####   ####''')
-print('''######   ##########|   |######   ##\   /##   ############''') 
-print('''######   #######(         )###   #########   #####   ####''')
-print('''#########################################################''')
-	    
-# compares new hashes to old hashes 
-for line in old_hashes:
-    if line in new_hashes:
-        print('good')
-    else:
-        print('%s ------> has been edited since last time this script ran' % line[0])		
+	# compares new hashes to old hashes 
+	for line in old_hashes:
+	    if line in new_hashes:
+		print('good')
+	    else:
+		print('%s ------> has been edited since last time this script ran' % line[0])		
